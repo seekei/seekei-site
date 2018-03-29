1ec57fc336ec81d22dcd41bb0835fa174e6d2a87
seekei <seekeilaw@foxmail.com>
2018-03-29 15:26:09 +0800

    首页全屏向下滚动效果

---
+++ public/js/index.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('#fullpage').fullpage({
+    fitToSection: true,
+    fitToSectionDelay: 10,
+    lazyLoading: true,
+    // 小点导航
+    navigation: true,
+    navigationTooltips: ['first', 'second', 'third', 'fourth']
+      // anchors: ['first', 'second', 'third', 'fourth'],
+  });
+})
